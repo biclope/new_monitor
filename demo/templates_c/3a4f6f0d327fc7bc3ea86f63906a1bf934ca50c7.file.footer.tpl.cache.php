6c4ef6e32a9656d35797893b69e34e6547cb876c
Yohan Kido <umax@live.ru>
2011-07-26 13:49:31 +0300

    initial commit

---
+++ demo/templates_c/3a4f6f0d327fc7bc3ea86f63906a1bf934ca50c7.file.footer.tpl.cache.php
@@ -0,0 +1,21 @@
+<?php /* Smarty version Smarty-3.0.8, created on 2011-07-26 13:30:27
+         compiled from "./templates/footer.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:3092595004e2e97435854e6-40336861%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '3a4f6f0d327fc7bc3ea86f63906a1bf934ca50c7' => 
+    array (
+      0 => './templates/footer.tpl',
+      1 => 1307128667,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '3092595004e2e97435854e6-40336861',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+)); /*/%%SmartyHeaderCode%%*/?>
+</BODY>
+</HTML>
